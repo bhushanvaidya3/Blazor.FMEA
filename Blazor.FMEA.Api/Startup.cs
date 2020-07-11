8c7ab9875fa5d50fdaff4bb2ed40bc2d46b53d04
Bhushan Vaidya <Bhushan.Vaidya@live.in>
2020-07-11 07:59:38 -0400

    Added Swagger

--- Blazor.FMEA.Api/Startup.cs
+++ Blazor.FMEA.Api/Startup.cs
@@ -21,6 +21,9 @@ namespace Blazor.FMEA.Api
         {
             services.AddControllers();
             services.AddTransient<ISiteMasterRepository, SiteMasterRepository>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +33,16 @@ namespace Blazor.FMEA.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
